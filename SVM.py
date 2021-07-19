3754cdbe41df46819b50e9c1267de94fc53a61ad
Jeannine Jordan <jjordan@lgamrica.com>
2021-07-19 19:31:37 -0400

    SVM.py

--- SVM.py
+++ SVM.py
@@ -383,13 +383,67 @@ plt.show()
 # Continuing from above
 X, y = make_moons(n_samples=100, noise=0.15, random_state=42)
 
+################### NOTE: Accessing a nested list cannot be done by multi-dimensional slicing
 def plot_dataset(X, y, axes):
     plt.plot(X[:, 0][y==0], X[:, 1][y==0], "bs")
     plt.plot(X[:, 0][y==1], X[:, 1][y==1], "g^")
-    plt.axis(axes)
+    plt.axis=(axes)
     plt.grid(True, which='both')
-    plt.xlabel(r"$x_1$'", fontsize=20)
-    plt.ylabel(r"$x_2$", fontsize=20)
+    plt.xlabel(r"$x_1$", fontsize=20)
+    plt.ylabel(r"$x_2$", fontsize=20, rotation=0)
 
 plot_dataset(X, y, [-1.5, 2.5, -1, 1.5])
 plt.show()
+
+polynomial_svm_clf = Pipeline([
+    ("poly_features", PolynomialFeatures(degree=3)),
+    ("scaler", StandardScaler()),
+    ("svm_clf", LinearSVC(C=10, loss="hinge", random_state=42))
+])
+
+polynomial_svm_clf.fit(X, y)
+
+def plot_predictions(clf, axes):
+    x0s = np.linspace(axes[0], axes[1], 100)
+    x1s = np.linspace(axes[2], axes[3], 100)
+    x0, x1 = np.meshgrid(x0s, x1s)
+    X = np.c_[x0.ravel(), x1.ravel()]
+    y_pred = clf.predict(X).reshape(x0.shape)
+    y_decision = clf.decision_function(X).reshape(x0.shape)
+    plt.contourf(x0, x1, y_pred, cmap=plt.cm.brg, alpha=0.2)
+    plt.contourf(x0, x1, y_decision, cmap=plt.cm.brg, alpha=0.1)
+
+plot_predictions(polynomial_svm_clf, [-1.5, 2.5, -1, 1.5])
+plot_dataset(X, y, [-1.5, 2.5, -1, 1.5])
+
+save_fig("moons_polynomial_svc_plot")
+plt.show()
+
+poly_kernel_svm_clf = Pipeline([
+    ('scaler', StandardScaler()),
+    ("svm_clf", SVC(kernel="poly", degree=3, coef0=1, C=5))
+])
+poly_kernel_svm_clf.fit(X, y)
+
+poly100_kernel_svm_clf = Pipeline([
+    ('scaler', StandardScaler()),``
+    ('svm_clf', SVC(kernel='poly', degree=10, coef0=100, C=5))
+])
+poly100_kernel_svm_clf.fit(X, y)
+
+fig, axes = plt.subplots(ncols=2, figsize=(10.5, 4), sharey=True)
+
+plt.sca(axes[0])
+plot_predictions(poly_kernel_svm_clf, [-1.5, 2.45, -1, 1.5])
+plot_dataset(X, y, [-1.5, 2.4, -1, 1.5])
+plt.title(r"$d=3, r=1, C=5$", fontsize=18)
+
+plt.sca(axes[1])
+plot_predictions(poly100_kernel_svm_clf, [-1.5, 2.45, -1, 1.5])
+plot_dataset(X, y, [-1.5, 2.4, -1, 1.5])
+plt.title(r"$d=10, r=100, C=5$", fontsize=18)
+plt.ylabel("")
+
+save_fig("moons_kernelized_polynomial_svc_plot")
+plt.show()
+
