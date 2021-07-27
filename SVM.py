a2ee77874843fe5e37715992aef06dc8b6968e64
Jeannine Jordan <jjordan@lgamrica.com>
2021-07-26 23:24:27 -0400

    SVM.py

--- SVM.py
+++ SVM.py
@@ -2,7 +2,7 @@
 # Reference: 'Hands-On Machine Learning with SciKit Learn & TensorFlow Concepts, Tools, and Techniques to Build Intelligent Systems' First Edition; Geron, Aurelien; O'Reilly, 2017
 
 # SVM - A 'most-popular' model, powerful and versatile for linear/non-linear classification, regression, outlier detection, and well-suited for classification of complex but small-medium sized data sets 
-
+# Import packages
 import os
 import numpy as np
 import pandas as pd
